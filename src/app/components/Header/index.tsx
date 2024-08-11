aaca38a724c4f4b0592b4e6d3a19fbab99b5256b
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-10 23:32:27 -0300

    feat: test Header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import styles from "../../page.module.css"
 import Link from "next/link";
+import { Head } from "./styles";
 
 export default function Header() {
   return (
-    <h1 style={{color: 'red'}}>Test Header</h1>
+    <Head>
+      <h1 style={{color: 'red'}}>Test Header</h1>
+
+    </Head>
   )
 }
---
+++ src/app/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Head = styled.div `
+  max-width: 100vw;
+  height: 6vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: blue;
+`
