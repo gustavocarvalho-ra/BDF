d31970ef07cdaad7fcbad87b167c1783f192be86
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-10 23:22:18 -0300

    feat: test component

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import styles from "../../page.module.css"
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <h1>Test Header</h1>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <h1>test</h1>
+    <>
+      <Header />
+      <h1>test</h1>
+    </>
   );
 }
