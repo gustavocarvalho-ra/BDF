ce60e105a41356bbdb930d252705413be4f757b1
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-18 21:43:05 -0300

    feat: test component

---
+++ src/app/components/Cadastro/index.tsx
@@ -0,0 +1,14 @@
+import { Cad } from "./styles"
+
+export default function Cadastro() {
+  return ( 
+    <Cad>
+      <div className="main">
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+    </Cad>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header";
+import Cadastro from "./components/Cadastro";
 
 export default function Home() {
   return (
     <>
       <Header />
       <h1>test</h1>
+      <Cadastro />
     </>
   );
 }
