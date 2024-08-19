3af92951291d3f2f9caa1729e42cb71f21f158f1
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-18 21:38:13 -0300

    feat: styled div

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import styles from "../../page.module.css"
 import Link from "next/link";
 import { Head } from "./styles";
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -16,13 +17,15 @@ export default function Header() {
           <Link href={"/"}>séríes</Link>
         </div>
 
-        <div className="login">
-          <button>
-            
+        <input className="inp" type="text" />
+
+
+        <div className="login" style={{marginLeft: "100px", fontSize: "50px"}}>
+          <button style={{background: "transparent", color: "red", border: "none", cursor: "pointer"}}>
+            <FaUserCircle style={{fontSize: "30px", background: "transparent"}}/>
           </button>
         </div>
 
-        <input className="inp" type="text" />
       </div>
     </Head>
   )
