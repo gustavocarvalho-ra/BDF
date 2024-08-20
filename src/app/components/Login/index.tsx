ee926544f4a9857e643881cbd0ae783c0831464e
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-19 21:49:34 -0300

    feat: login component

---
+++ src/app/components/Login/index.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { FaUser, FaLock } from "react-icons/fa";
+
+export default function Login() {
+  return(
+    <div className="main">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder='Nome de Usuário' required />
+          <FaUser className='icon' />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder='Senha' required />
+          <FaLock className='icon' />
+
+        </div>
+
+        <div className="esqueceu-senha">
+          <label><input type="checkbox" />Salvar a senha</label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+
+        <button type='submit'>Login</button>
+
+        <div className="register-link">
+          <p>Não possui conta? <a href="#">Registrar</a></p>
+        </div>
+      </form>
+    </div>
+  )
+}
