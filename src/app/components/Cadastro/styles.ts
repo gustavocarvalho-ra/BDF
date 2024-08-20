0021447a1a85057457a9245c82569c625707288b
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-19 21:45:12 -0300

    feat: test main Cad

--- src/app/components/Cadastro/styles.ts
+++ src/app/components/Cadastro/styles.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const Cad = styled.div `
-  width: 40vw;
+  width: 20vw;
   height: 30vh;
   background: red;
+
+  .main {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+
+    input {
+      width: 80%;
+      height: 3em;
+      
+    }
+  }
 `
