8e7d6ea9351d2b68a2fa1864665008ef1eec9016
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-19 23:51:58 -0300

    feat: test button

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import styles from "../../page.module.css"
+
+import { useState } from "react";
 import Link from "next/link";
+
 import { Head } from "./styles";
 import { FaUserCircle } from "react-icons/fa";
 
 export default function Header() {
+
+  const [isLog, setLog] = useState(false)
+
+  const LoginPop = () => {
+    setLog((prevState) => !prevState)
+  }
+
   return (
     <Head>
       <Link className="title" href={"/"} style={{color: 'red'}}>Test Header</Link>
@@ -21,7 +30,7 @@ export default function Header() {
 
 
         <div className="login" style={{marginLeft: "100px", fontSize: "50px"}}>
-          <button style={{background: "transparent", color: "red", border: "none", cursor: "pointer"}}>
+          <button onClick={LoginPop} style={{background: "transparent", color: "red", border: "none", cursor: "pointer"}}>
             <FaUserCircle style={{fontSize: "30px", background: "transparent"}}/>
           </button>
         </div>
