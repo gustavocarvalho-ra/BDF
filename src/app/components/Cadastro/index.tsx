985647ef4fd791b35483a50727d3a868618c1bc9
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-23 18:40:17 -0300

    feat: test wrapper

--- src/app/components/Cadastro/index.tsx
+++ src/app/components/Cadastro/index.tsx
@@ -4,28 +4,33 @@ import { MdEmail } from "react-icons/md";
 import { FaUser, FaLock } from "react-icons/fa";
 
 export default function Cadastro() {
-  const wrapper = document.querySelector('.wrapper')
-const loginLink = document.querySelector('.login-link')
-const registerLink = document.querySelector('.register-link')
-const btnPopup = document.querySelector('.btnLogin-popup')
-const iconClose = document.querySelector('.icon-close')
+  const wrapper = document.querySelector('.wrapper') as HTMLElement;
+  const loginLink = document.querySelector('.login-link') as HTMLElement;
+  const registerLink = document.querySelector('.register-link') as HTMLElement;
+  const btnPopup = document.querySelector('.btnLogin-popup') as HTMLElement;
+  const iconClose = document.querySelector('.icon-close') as HTMLElement;
+  
+  // registerLink.addEventListener('click', () => {
+  //   wrapper.classList.add('active');
+  // });
+  
+  // loginLink.addEventListener('click', () => {
+  //   wrapper.classList.remove('active');
+  // });
+  
+  // btnPopup.addEventListener('click', () => {
+  //   wrapper.classList.add('active-popup');
+  // });
+  
+  // iconClose.addEventListener('click', () => {
+  //   wrapper.classList.remove('active-popup');
+  // });
 
+  // if (wrapper) {
+  //   wrapper.classList.add('active');
+  // }
+  
 
-registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
-})
-
-loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
-})
-
-btnPopup.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
-})
-
-iconClose.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup');
-})
   return ( 
     <Cad>
       
--- src/app/components/Login/index.tsx
+++ src/app/components/Login/index.tsx
@@ -7,58 +7,61 @@ import Image from "next/image";
 import { Cad } from "../Cadastro/styles";
 
 export default function Login() {
-  const wrapper = document.querySelector('.wrapper')
-const loginLink = document.querySelector('.login-link')
-const registerLink = document.querySelector('.register-link')
-const btnPopup = document.querySelector('.btnLogin-popup')
-const iconClose = document.querySelector('.icon-close')
+  const wrapper = document.querySelector('.wrapper') as HTMLElement;
+  const loginLink = document.querySelector('.login-link') as HTMLElement;
+  const registerLink = document.querySelector('.register-link') as HTMLElement;
+  const btnPopup = document.querySelector('.btnLogin-popup') as HTMLElement;
+  const iconClose = document.querySelector('.icon-close') as HTMLElement;
+  
+  // registerLink.addEventListener('click', () => {
+  //   wrapper.classList.add('active');
+  // });
+  
+  // loginLink.addEventListener('click', () => {
+  //   wrapper.classList.remove('active');
+  // });
+  
+  // btnPopup.addEventListener('click', () => {
+  //   wrapper.classList.add('active-popup');
+  // });
+  
+  // iconClose.addEventListener('click', () => {
+  //   wrapper.classList.remove('active-popup');
+  // });
 
-
-registerLink.addEventListener('click', () => {
-  wrapper.classList.add('active');
-})
-
-loginLink.addEventListener('click', () => {
-  wrapper.classList.remove('active');
-})
-
-btnPopup.addEventListener('click', () => {
-  wrapper.classList.add('active-popup');
-})
-
-iconClose.addEventListener('click', () => {
-  wrapper.classList.remove('active-popup');
-})
+  // if (wrapper) {
+  //   wrapper.classList.add('active');
+  // }
 
   return(
     <Cad>
       <div className="wrapper">
 
         <div className="form-box login">
-        <h2>Login</h2>
-        <form action="#">
-          <div className="input-box">
-            <span className="icon"><MdEmail name="mail"></MdEmail></span>
-            <input type="e-mail" required/>
-            <label>Email</label>
-          </div>
-          <div className="input-box">
-            <span className="icon"><FaLock name="lock-closed"></FaLock></span>
-            <input type="password" required/>
-            <label>Senha</label>
-          </div>
-          <div className="remember-forgot">
-            <label><input type="checkbox"/>Lembrar senha</label>
-            <a href="#">Esqueceu a senha?</a>
-          </div>
-          <button type="submit" className="btn">Entrar</button>
-          <div className="login-register">
-            <p>Não possui conta?
-              <a href="#" className="register-link">Registrar</a>
-            </p>
-          </div>
-        </form>
-      </div>
+          <h2>Login</h2>
+          <form action="#">
+            <div className="input-box">
+              <span className="icon"><MdEmail name="mail"></MdEmail></span>
+              <input type="e-mail" required/>
+              <label>Email</label>
+            </div>
+            <div className="input-box">
+              <span className="icon"><FaLock name="lock-closed"></FaLock></span>
+              <input type="password" required/>
+              <label>Senha</label>
+            </div>
+            <div className="remember-forgot">
+              <label><input type="checkbox"/>Lembrar senha</label>
+              <a href="#">Esqueceu a senha?</a>
+            </div>
+            <button type="submit" className="btn">Entrar</button>
+            <div className="login-register">
+              <p>Não possui conta?
+                <a href="#" className="register-link">Registrar</a>
+              </p>
+            </div>
+          </form>     
+        </div>
       </div>
     </Cad>
   )
